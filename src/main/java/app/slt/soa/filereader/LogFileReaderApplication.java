6ada6ae230e7fbc535f0233e2923a405622b410f
ipranasinghe <52536005+ipranasinghe@users.noreply.github.com>
2020-06-29 11:24:17 +0530

    Initial changes

---
+++ src/main/java/app/slt/soa/filereader/LogFileReaderApplication.java
@@ -0,0 +1,13 @@
+package app.slt.soa.filereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogFileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFileReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/slt/soa/filereader/FilereaderApplicationTests.java
@@ -0,0 +1,13 @@
+package app.slt.soa.filereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilereaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
