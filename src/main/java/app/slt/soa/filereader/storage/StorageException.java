570ae7b363b2d2548fd80573611117fd8482bdd9
ipranasinghe <52536005+ipranasinghe@users.noreply.github.com>
2020-07-12 16:13:16 +0530

    change uploading

---
+++ src/main/java/app/slt/soa/filereader/storage/StorageException.java
@@ -0,0 +1,12 @@
+package app.slt.soa.filereader.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ src/main/java/app/slt/soa/filereader/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package app.slt.soa.filereader.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
