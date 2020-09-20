ba81a6a314d8f27f155cf10d1ef98286db2f2622
Prasadika Ranasinghe <i.prasadika.ranasinghe@gmail.com>
2020-09-20 08:47:06 -0700

    minor changes

--- src/main/java/app/slt/soa/filereader/services/MyUserDetailsService.java
+++ src/main/java/app/slt/soa/filereader/services/MyUserDetailsService.java
@@ -12,8 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
