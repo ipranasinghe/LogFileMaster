ad2d7e5592cd33ae193041ca3e321023c4714b78
Prasadika Ranasinghe <i.prasadika.ranasinghe@gmail.com>
2020-09-20 09:09:35 -0700

    minor changes

---
+++ src/main/java/lk/slt/soa/logfilesmaster/LogFilesMasterApplication.java
@@ -0,0 +1,27 @@
+package lk.slt.soa.logfilesmaster;
+
+import lk.slt.soa.logfilesmaster.storage.StorageProperties;
+import lk.slt.soa.logfilesmaster.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class LogFilesMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFilesMasterApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
